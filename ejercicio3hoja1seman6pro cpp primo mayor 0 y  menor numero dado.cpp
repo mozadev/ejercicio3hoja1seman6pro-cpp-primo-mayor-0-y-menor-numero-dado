1ba822b72a84d1b4bcadcfea65baa3c5116a1901
CesarAroon <30559667+CesarAroon@users.noreply.github.com>
2020-12-15 21:53:29 -0500

    Add files via upload

---
+++ ejercicio3hoja1seman6pro cpp primo mayor 0 y  menor numero dado.cpp
@@ -0,0 +1,48 @@
+﻿/*
+
+Escriba un programa en C++ que imprima todos los múltiplos de 3, mayores que cero,
+que sean menores que un número N (positivo) que será ingresado como dato.
+Ejemplo:
+Ingrese N: 25
+Los múltiplos son: 3 6 9 12 15 18 21 24
+
+*/
+#include <iostream>
+#include<conio.h>
+#include <stdlib.h>
+using namespace std;
+
+int ingresenum()
+
+{
+	int num;
+	cout << "ingrese numero: "; cin >> num;
+	while (num <= 0)
+	{cout << "ingrese numero: "; cin >> num;}
+	return num;
+}
+
+
+void generamultiplos(int num)
+
+{
+	int i = 1;
+		do
+	{
+		if(i%3==0)
+		cout <<i << "\t";
+		i++;
+	}
+	while (i<=num);
+}
+int main()
+{
+	int n;
+	n = ingresenum();
+	cout << "la serie: ";
+	generamultiplos(n);
+	_getch();
+}
+
+
+
